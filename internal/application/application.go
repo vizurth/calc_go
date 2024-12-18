a4009e7df0593f361ab6a78587aebeb5f5ce36a8
vizurth <timrsamara@gmail.com>
2024-12-19 00:05:21 +0300

    add application and main go file

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,3 +1,60 @@
 package application
 
-import "github.com/vizurth/calc_go/pkg/calc"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/vizurth/calc_go/pkg/calc"
+)
+
+type Config struct{
+	Addr string
+}
+
+func ConfigFromEnv() *Config{
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == ""{
+		config.Addr = "8080"
+	}
+	return config
+}
+
+type Application struct{
+	config *Config
+}
+
+func New() *Application{
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+type Request struct{
+	Expression string
+}
+
+
+func CalculateHandle(w http.ResponseWriter, r *http.Request){
+	request := new(Request)
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := calc.Calc(request.Expression)
+	if err != nil{
+		fmt.Fprintf(w, "err: %s", err.Error())
+	} else {
+		fmt.Fprintf(w, "result: %f", result)
+	}
+}
+
+func (a *Application) RunServer() error {
+	http.HandleFunc("/api/v1/calculation", CalculateHandle)
+	return http.ListenAndServe(":" + a.config.Addr, nil)
+}
